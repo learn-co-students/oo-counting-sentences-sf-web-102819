ddd44b760da607d3b95ce0585ca4c05ac1bd4022
github username <github email address>
2019-10-29 22:32:01 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    #https://stackoverflow.com/questions/42216136/count-the-number-of-sentences-in-a-paragraph-using-ruby
+    self.split(/[.?!]+/).count
   end
 end
